f327a9a9ae4ade86bfc161f5ba15d642879cad37
Kit Fuderich <kitfuderich@gmail.com>
2020-06-01 09:24:21 -0700

    Week 3 Assignment Completed - React Form as Custom Component

--- src/components/CommentFormComponent.js
+++ src/components/CommentFormComponent.js
@@ -1,91 +1,9 @@
-
 import React, {Component} from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody,Label, Row, Col } from 'reactstrap';
-import { Link } from 'react-router-dom';
-
-
-
-
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
-function RenderDish({dish}){
- return (   
-
-<Card key={dish.id}>
-    <CardImg width="100%" top src={dish.image} alt={dish.name} />
-    <CardTitle>{dish.name}</CardTitle>
-    <CardText>{dish.description}</CardText>   
-</Card> 
-
- )}
-
-
-
-function RenderComments({comments}){  
-const rencomment = comments.map((info) => 
-      
-             <ol key={info.id}>
-             {console.log(info.id)}
-             <p>{info.comment}</p>
-             <p>-- {info.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(info.date)))}</p>
-            </ol>  
-
-              
-    );
-
-
-if(comments != null){
-    console.log("about to return JSX")    
-    return(
-    <div>
-     {rencomment} 
-    <CommentForm></CommentForm>    
-    </div>
-   
-    )
-     
-}
-else{
-    return <div></div>;
-}
-}
-
-
-const DishDetail = (props)=>{
-
-if (props.dish != null)
-
-return( 
-    <div className="container">
-    <div className="row">
-        <Breadcrumb>
-            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-            <h3>{props.dish.name}</h3>
-            <hr />
-        </div>                
-    </div>
-    <div className="row">
-        <div className="col-12 col-md-5 m-1">
-            <RenderDish dish={props.dish} />
-        </div>
-        <div className="col-12 col-md-5 m-1">
-            <RenderComments comments={props.comments} />
-        </div>
-    </div>
-    </div>
-
-);
-
-else
-return(
-    <div></div>
-);
-
-}
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -210,6 +128,4 @@ render(){
 }
 
 
-
-export default DishDetail;
-
+export default CommentForm;
