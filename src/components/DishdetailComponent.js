4c258af8957725f72b7084b570c1f5a6c5ed2b0e
Kit Fuderich <kitfuderich@gmail.com>
2020-06-01 16:19:33 -0700

    Redux Thunk Example (spinner/loader)

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody,Label, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { Loading } from './LoadingComponent';
 
 
 
@@ -54,8 +54,25 @@ else{
 
 
 const DishDetail = (props)=>{
-
-if (props.dish != null)
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+else if (props.dish != null)
 
 return( 
     <div className="container">
