84c6be692947982bac53db2e13f6d753dbff6e13
Kit Fuderich <kitfuderich@gmail.com>
2020-06-02 11:26:54 -0700

    Fetch from Server Example - setup example with json-server on local 3001

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,7 +5,7 @@ import { Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody,Label, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 
 import { Control, LocalForm, Errors } from 'react-redux-form';
@@ -14,7 +14,7 @@ function RenderDish({dish}){
  return (   
 
 <Card key={dish.id}>
-    <CardImg width="100%" top src={dish.image} alt={dish.name} />
+    <CardImg width="100%" top src={baseUrl + dish.image} alt={dish.name} />
     <CardTitle>{dish.name}</CardTitle>
     <CardText>{dish.description}</CardText>   
 </Card> 
