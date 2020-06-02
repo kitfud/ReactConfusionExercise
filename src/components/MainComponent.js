cf255b360e1696eaaaf2824576b8344524a4e20a
Kit Fuderich <kitfuderich@gmail.com>
2020-06-02 14:54:24 -0700

    React Animations Example- slide in transitions between routes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,6 +13,7 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStateToProps = state => {
   return {
@@ -83,7 +84,9 @@ class Main extends Component {
       <div>
       <Header />
       <div>
-        <Switch>
+      <TransitionGroup>
+      <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+        <Switch location={this.props.location}>
             <Route path='/home' component={HomePage} />
             <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
             <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
@@ -91,6 +94,8 @@ class Main extends Component {
             <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
             <Redirect to="/home" />
         </Switch>
+        </CSSTransition>
+        </TransitionGroup>
       </div>
       <Footer />
     </div>
