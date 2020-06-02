981d675d60fe7b8c35c9b01a94fc3b67118a105d
Kit Fuderich <kitfuderich@gmail.com>
2020-06-01 17:14:16 -0700

    React Redux Forms Revisited- info persists between windows before clicking submit

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Breadcrumb, BreadcrumbItem,
     Button, Row, Col, Label } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
 
@@ -24,6 +24,7 @@ class Contact extends Component {
     handleSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
         // event.preventDefault();
     }
     
@@ -80,7 +81,7 @@ render(){
             
              <div className="col-12 col-md-9">
     
-             <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+             <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
              <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -205,7 +206,7 @@ render(){
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                  
 
 
