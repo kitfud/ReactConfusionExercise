f338cb58ebcbff2ad7d4d91392af2f4fd90855b0
Kit Fuderich <kitfuderich@gmail.com>
2020-06-03 16:36:10 -0700

    package fixed

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -94,7 +94,12 @@ class Main extends Component {
       <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
         <Switch location={this.props.location}>
             <Route path='/home' component={HomePage} />
-            <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
+            <Route exact path='/aboutus' component={() => <About 
+             leaders={this.props.leaders} 
+             leaderLoading={this.props.leaders.isLoading}
+             leaderErrMess={this.props.leaders.errMess}
+            />} />} />
+            
             <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}  postFeedback={this.props.postFeedback}/>} />
